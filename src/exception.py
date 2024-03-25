10542cb266d4dc44cf5ea92fe49a040047001caf
Rishav Ranaut <rishav.ranaut82@gmail.com>
2024-03-26 00:25:08 +0530

    data transformation done

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -19,15 +18,7 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-
-
-# if __name__=="__main__":
-
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("divide by zero")
-#         raise CustomException(e,sys)
+    
 
 
         
